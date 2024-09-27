312b3aab8f19b9d7f3a5b37a426129b835aadd23
Tony <145759988+RyZe1777@users.noreply.github.com>
2024-09-27 22:04:07 +0000

    Update

--- data_base.cpp
+++ data_base.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream> //biblioteca para ler (ifstream) e para criar e escrever (ofstream) ficheiros
+#include <fstream> //biblioteca para ler (ifstream) // para criar e escrever (ofstream) ficheiros
  
 using namespace std;
 
@@ -12,13 +12,45 @@ struct Produto
     int quantidade;
 };
 
+int criar_DataBase(){
+    
+    int escolha;
+    ifstream ficheiro("Database.csv"); //variavel do tipo ifstream com o nome de ficheiro //Database é nome do arquivo que vamos usar para guardar os dados
+    
+    //se ja tivermos uma database
+    if (ficheiro){ 
+        cout << "Ja tens uma Base de Dados criada" <<endl;
+    //senao tivermos uma database
+    }else{
+        cout << "Deseja criar um nova Base de Dados?" <<endl;
+        cout << "1 - Sim" <<endl;
+        cout << "2 - Nao" <<endl;
+        cin >> escolha;
+        
+        if (escolha == 1){
+            ofstream ficheiro_1("Database.csv"); //variavel do tipo ifstream com o nome de ficheiro_1
+            if(ficheiro_1){
+                cout<< "A sua Base de Dados foi criada com sucesso!" <<endl;
+            }else{
+                cout << "Ocorreu um erro na Criação da sua Base de Dados" <<endl;
+            }
+        }else if(escolha == 2){
+            return 0;
+        }
+    }
+}
+
+
+
+
+
 void menu(){
     system("clear");
     cout << "\n==========================" <<endl;
     cout << "          MENU" <<endl;
     cout << "==========================" <<endl;
-    cout << "1-Adicionar Produto"
-    cout << "2-Consultar Produto <<endl;
+    cout << "1-Adicionar Produto" <<endl;
+    cout << "2-Consultar Produto" <<endl; //penso que ja esteja pronto not sure
     cout << "3-Alterar Produto" <<endl;
     cout << "4-Eliminar Produto" <<endl;
     cout << "5-SAIR" <<endl;
@@ -49,7 +81,7 @@ do{
         break;
 
     case 2://consultar
-        
+        criar_DataBase; // maybe posiçao errada
         break;
 
     case 3://alterar
